b83ac17a263f04a1a590e78f8f479dae649a7be0
Sergey Malk <errorinever@gmail.com>
2020-01-30 06:42:08 +0300

    add clear db for tests

--- handlers.py
+++ handlers.py
@@ -1,6 +1,6 @@
 from model.bot import bot
 from telebot import apihelper
-from utils import cmd
+from utils import cmd, data_base
 from utils.cfg import cfg
 from model import functions
 
@@ -11,21 +11,29 @@ def extract_args(arg):
 
 @bot.message_handler(commands=['status'])
 def start_message(message):
-    """check auth"""
+    """get name"""
     bot.send_message(message.chat.id, bot.get_me())
+
+
+@bot.message_handler(commands=['clear_db'])
+def clear_database(message):
+    """drop table"""
+    data_base.clear_table()
+    bot.send_message(message.chat.id, 'DB is empty')
 
 
 @bot.message_handler(commands=['convert'])
 def convert_voice_msg_user(message):
-    """
-    convert all voice message of user
-    /convert uid """
+    """convert all voice message of user to wav"""
     uid = extract_args(message.text)[0]
     functions.convert_to_wav(uid)
 
 
 @bot.message_handler(content_types=['voice', 'photo'])
 def get_attachments(message):
+    """ Receives voice message or photo. If it's a voice then saves it,
+    else if it's a photo then checks whether there is a face, and if there is then saves it.
+    """
     user_id = message.from_user.id
 
     if message.content_type == 'voice':
@@ -33,7 +41,8 @@ def get_attachments(message):
         functions.save_voice(user_id, file_id)
 
     elif message.content_type == 'photo':
-        pass
+        file_id = message.photo[-1].file_id
+        functions.save_img(user_id, file_id)
 
 
 if __name__ == '__main__':
