5ed2b86c298065364554bf9f3b6b7332c3538f78
Sergey Malk <errorinever@gmail.com>
2020-01-30 06:44:17 +0300

    reformat

--- utils/cfg.py
+++ utils/cfg.py
@@ -1,17 +1,23 @@
 from easydict import EasyDict as edict
 
 __C = edict()
+
+
 # Consumers can get config by:
 cfg = __C
 
+
 # API TOKEN
 __C.TOKEN = None
-# OS
+
+# path to directory where all files will be stored
 __C.OUTDIR = None
+# path to directory where converted files will be stored
 __C.OUT_DIR_WAV = None
 
-# DATABASE
+# path to database
 __C.DB_PATH = r'uid.db'
+
 
 # Table scheme
 # CREATE TABLE voices (
@@ -20,6 +26,7 @@ __C.DB_PATH = r'uid.db'
 # 	voice_path TEXT NOT NULL
 # );
 
+# SQL queries
 __C.SQL_INSERT_QUERY_VOICE = """INSERT INTO voices (uid, voice_path) VALUES (?, ?)"""
 __C.SQL_SELECT_QUERY_VOICE = """SELECT voice_path FROM voices WHERE uid == (?)"""
 __C.SQL_CLEAR_TABLE = """DELETE FROM voices"""
